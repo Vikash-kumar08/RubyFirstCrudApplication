c99c39d7d429415b584676b9c35b3e50c4fc8f21
vikashkumar0835 <vikash.yadav@jarvis.consulting>
2021-10-08 19:05:14 +0530

    CRUDapp

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,52 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+
+
+
+  def create
+    #render plain: params[:article]
+    @article = Article.new(article_params)
+    if @article.save!
+        flash[:notice] = "Article was created successfully."
+        redirect_to article_path(@article.id)
+    else
+      render 'new'
+    end
+  end
+
+
+
+     def edit
+       @article = Article.find(params[:id])
+     end
+
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        flash[:notice] = "Article was updated successfully."
+        redirect_to articles_path
+      else
+        render 'edit'
+      end
+    end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
